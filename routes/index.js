1bf4a3041a7d28c94f6102837c0d2ab2af8374ad
OliverEklund <oliver.eklund@elev.ga.ntig.se>
2025-11-26 10:16:35 +0100

    klar

--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,7 @@ const router = express.Router()
 router.get("/", (req, res) => {
     res.render("index.njk", {
         title: "Välkommen till ett soloäventyr",
-        message: "En Tristan & Isolde Remix! Detta är ett skolarbete"
+        message: "En postapokalyptisk Tristan & Isolde Remix! Detta är ett skolarbete"
     })
 })
 
